7820e8a03d562356ec5d2cfe59e7eb0eceb53ead
Krishnapriya MU <krishnapriya.mu@infospica.com>
2023-07-16 21:35:44 +0530

    New file added

---
+++ HyperMart.java
@@ -0,0 +1,19 @@
+public class HyperMart implements FrontDesk {
+  public void enquiry() {
+    System.out.println("enquiry is ");
+  }
+
+  public void Selection() {
+    System.out.println("Seletced");
+  }
+
+  public void printReceipt() {
+    System.out.println("Receipt is ");
+
+  }
+
+  public void getMoney() {
+    System.out.println("Money is ");
+
+  }
+}
