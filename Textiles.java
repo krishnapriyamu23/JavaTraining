a0796b5862f89c4bc1b5724978ddb7815d67abca
Krishnapriya MU <krishnapriya.mu@infospica.com>
2023-07-16 21:37:00 +0530

    New file added

---
+++ Textiles.java
@@ -0,0 +1,17 @@
+public class Textiles implements Billing {
+  public void getMoney() {
+    System.out.println("The total amount is:");
+  }
+
+  public void KidsSection() {
+    System.out.println("Kids of ahe 5-9");
+
+  }
+
+  public void printReceipt() {
+    System.out.println("Printing the amount");
+
+  }
+  
+
+}
